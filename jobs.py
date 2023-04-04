8105f47c25372ee9fdc902ed4d4d327e7ae92198
Jimil Desai <jimildesai42@gmail.com>
2023-04-04 17:02:37 +0200

    retrieve buckets from cronjob specs
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- jobs.py
+++ jobs.py
@@ -1,5 +1,14 @@
 # this job should list the buckets contents and create 64 jobs each job backing up files
-
 import logging
+import os
 
+from kubernetes import client, config
+
+config.load_incluster_config()
+api = client.BatchV1beta1Api()
+
+# get source buckets from the cronjob specs
+source_buckets = os.environ.get("BUCKET_LIST")
+bucket = source_buckets.split(",")
+logging.info(bucket)
 logging.info("job is created!!")
