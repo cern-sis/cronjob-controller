ab739bda6b8c4a150a2847ed883bf0b1c45be91c
Jimil Desai <jimildesai42@gmail.com>
2023-04-28 11:10:24 +0200

    debug lines for completed jobs
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- jobs.py
+++ jobs.py
@@ -61,16 +61,19 @@ def backup(bucket_name):
             if job.status.succeeded is not None or job.status.failed is not None
         ]
 
-        # cleanup completed jobs
+        print(f"iteration: {page_num}")
         for jobs in completed_jobs:
-            print(f"{jobs.metadata.name} is completed - deleting")
-            try:
-                batchAPI.delete_namespaced_job(
-                    name=jobs.metadata.name, namespace=os.environ["NAMESPACE"]
-                )
-            except client.rest.ApiException as e:
-                if e.status == 404:
-                    print(f"{jobs.metadata.name} deleted")
+            print(f"{job.metadata.name} is completed")
+        # # cleanup completed jobs
+        # for jobs in completed_jobs:
+        #     print(f"{jobs.metadata.name} is completed - deleting")
+        #     try:
+        #         batchAPI.delete_namespaced_job(
+        #             name=jobs.metadata.name, namespace=os.environ["NAMESPACE"]
+        #         )
+        #     except client.rest.ApiException as e:
+        #         if e.status == 404:
+        #             print(f"{jobs.metadata.name} deleted")
 
         # wait if running jobs exceeds the max parallel jobs
         if len(running_jobs) >= int(os.environ["TOTAL_JOBS"]):
